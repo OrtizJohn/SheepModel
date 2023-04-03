15cd01a4e4a42f175c5c2093685fcadad32f8fa9
John A Ortiz <johno2k35@gmail.com>
2023-04-02 22:10:05 -0600

    visualizations added of sheep zones
    Could not add anim.html as it is to lg in size to push will run locally is same as Noah's branch anim.html however logged with new visualizations

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import numpy as np
 
 # Visualization
 import matplotlib.pyplot as plt
+import matplotlib as mp
 import webbrowser
 
+mp.rcParams['animation.embed_limit'] = 2**25
 
 def normalize(v):
     """ Normalize a vector to length 1. """
@@ -15,96 +17,6 @@ def normalize(v):
     if norm == 0:
         return v
     return v / norm
-
-
-class Boid(ap.Agent):
-    """ An agent with a position and velocity in a continuous space,
-    who follows Craig Reynolds three rules of flocking behavior;
-    plus a fourth rule to avoid the edges of the simulation space. """
-
-    def setup(self):
-
-        self.velocity = normalize(
-            self.model.nprandom.random(self.p.ndim) - 0.5)
-
-    def setup_pos(self, space):
-
-        self.space = space
-        self.neighbors = space.neighbors
-        self.pos = space.positions[self]
-
-    def update_velocity(self):
-
-        pos = self.pos
-        ndim = self.p.ndim
-
-        # Rule 1 - Cohesion
-        nbs = self.neighbors(self, distance=self.p.outer_radius)
-        nbs_len = len(nbs)
-        nbs_pos_array = np.array(nbs.pos)
-        nbs_vec_array = np.array(nbs.velocity)
-        if nbs_len > 0:
-            center = np.sum(nbs_pos_array, 0) / nbs_len
-            v1 = (center - pos) * self.p.cohesion_strength
-        else:
-            v1 = np.zeros(ndim)
-
-        # Rule 2 - Separation
-        v2 = np.zeros(ndim)
-        for nb in self.neighbors(self, distance=self.p.inner_radius):
-            v2 -= nb.pos - pos
-        v2 *= self.p.separation_strength
-
-        # Rule 3 - Alignment
-        if nbs_len > 0:
-            average_v = np.sum(nbs_vec_array, 0) / nbs_len
-            v3 = (average_v - self.velocity) * self.p.alignment_strength
-        else:
-            v3 = np.zeros(ndim)
-
-        # Rule 4 - Borders
-        v4 = np.zeros(ndim)
-        d = self.p.border_distance
-        s = self.p.border_strength
-        for i in range(ndim):
-            if pos[i] < d:
-                v4[i] += s
-            elif pos[i] > self.space.shape[i] - d:
-                v4[i] -= s
-
-        # Update velocity
-        self.velocity += v1 + v2 + v3 + v4
-        self.velocity = normalize(self.velocity)
-
-    def update_position(self):
-
-        self.space.move_by(self, self.velocity)
-
-
-class BoidsModel(ap.Model):
-    """
-    An agent-based model of animals' flocking behavior,
-    based on Craig Reynolds' Boids Model [1]
-    and Conrad Parkers' Boids Pseudocode [2].
-
-    [1] http://www.red3d.com/cwr/boids/
-    [2] http://www.vergenet.net/~conrad/boids/pseudocode.html
-    """
-
-    def setup(self):
-        """ Initializes the agents and network of the model. """
-
-        self.space = ap.Space(self, shape=[self.p.size] * self.p.ndim)
-        self.agents = ap.AgentList(self, self.p.population, Boid)
-        self.space.add_agents(self.agents, random=True)
-        self.agents.setup_pos(self.space)
-
-    def step(self):
-        """ Defines the models' events per simulation step. """
-
-        self.agents.update_velocity()  # Adjust direction
-        self.agents.update_position()  # Move into new direction
-
 
 class Sheep(ap.Agent):
     """ An agent with a position and velocity in a continuous space,
@@ -116,7 +28,7 @@ class Sheep(ap.Agent):
         # self.velocity = normalize(
         #     self.model.nprandom.random(self.p.ndim) - 0.5)
         self.velocity = normalize(np.array([0.0, 0.0]))
-
+        self.type = "Sheep"
     def setup_pos(self, space):
 
         self.space = space
@@ -209,6 +121,8 @@ class Sheep(ap.Agent):
 
         self.space.move_by(self, self.velocity)
 
+        
+
 
 class SheepModel(ap.Model):
     """
@@ -227,6 +141,7 @@ class SheepModel(ap.Model):
         self.agents = ap.AgentList(self, self.p.population, Sheep)
         self.space.add_agents(self.agents, random=True)
         self.agents.setup_pos(self.space)
+        
 
     def step(self):
         """ Defines the models' events per simulation step. """
@@ -235,53 +150,50 @@ class SheepModel(ap.Model):
         self.agents.update_position()  # Move into new direction
 
 
-def animation_plot_single(m, ax):
+def animation_plot_single(m, ax): # for a single stepp
+    #print(m.type)
     ndim = m.p.ndim
-    ax.set_title(f"Boids Flocking Model {ndim}D t={m.t}")
+    ax.set_title(f"Sheep Flocking Model {ndim}D t={m.t}")
     pos = m.space.positions.values()
     pos = np.array(list(pos)).T  # Transform
     ax.scatter(*pos, s=1, c='black')
+    
+
+    #updating each agent
+    for agent in m.agents:
+        if(agent.type == "Sheep"):
+            #TODO: drawing boundaries for each zone ( zone1 repulsive, zone2 nuetral, zone3 attractive, zone4 nothing as out of sight)
+            c1 = plt.Circle((agent.pos[0],agent.pos[1]),radius=agent.p.radius_one,edgecolor="red",facecolor=(0,0,0,0)) #border of zone 1 and zone 2 
+            c2 = plt.Circle((agent.pos[0],agent.pos[1]),radius=agent.p.radius_two,edgecolor="yellow",facecolor=(0,0,0,0)) #border of zone 2 and zone 3
+            c3 = plt.Circle((agent.pos[0],agent.pos[1]),radius=agent.p.radius_three,edgecolor="pink",facecolor=(0,0,0,0)) #border of zone 3 and 4
+            ax.add_patch(c1)
+            ax.add_patch(c2)
+            ax.add_patch(c3)
+
+
     ax.set_xlim(0, m.p.size)
     ax.set_ylim(0, m.p.size)
-    if ndim == 3:
-        ax.set_zlim(0, m.p.size)
     ax.set_axis_off()
 
-
 def animation_plot(m, p):
-    projection = '3d' if p['ndim'] == 3 else None
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111, projection=projection)
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(111, projection=None)
+
     animation = ap.animate(m(p), fig, ax, animation_plot_single)
     return animation.to_jshtml(fps=30)
 
 
-parameters2D = {
+parameters = {
     'size': 50,
     'seed': 123,
     'steps': 200,
     'ndim': 2,
-    'population': 200,
-    'inner_radius': 3,
-    'outer_radius': 10,
-    'border_distance': 10,
-    'cohesion_strength': 0.005,
-    'separation_strength': 0.1,
-    'alignment_strength': 0.3,
-    'border_strength': 0.5
-}
-
-parameters2DAlt = {
-    'size': 50,
-    'seed': 123,
-    'steps': 200,
-    'ndim': 2,
-    'population': 200,
+    'population': 50,
     'radius_one': 2,
-    'radius_two': 10,
-    'radius_three': 20,
+    'radius_two': 5,
+    'radius_three': 10,
     'border_distance': 10,
-    'inner_radius': 3,
+    'inner_radius': 3,   
     'outer_radius': 10,
     'cohesion_strength': 0.005,
     'separation_strength': 0.1,
@@ -289,7 +201,8 @@ parameters2DAlt = {
     'border_strength': 0.5
 }
 
-html = animation_plot(SheepModel, parameters2DAlt)
+
+html = animation_plot(SheepModel, parameters)
 
 path = os.path.abspath('anim.html')
 url = 'file://' + path
