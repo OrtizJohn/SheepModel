a804e10ce3120292612460129ddba0f2b2831416
John A Ortiz <johno2k35@gmail.com>
2023-04-12 17:16:30 -0600

    added anchor points

--- main.py
+++ main.py
@@ -80,8 +80,12 @@ class Dog(ap.Agent):
     def setup(self):
 
         # self.velocity = np.zeros(2)
-        self.velocity = np.array([-0.5, 0.5])
+        self.velocity = np.array([0.0, 0.0])
         self.state_flag = 1
+        self.V_lf = None
+        self.V_rf = None
+        self.V_la = None
+        self.V_ra = None
 
     def setup_pos(self, space):
 
@@ -100,6 +104,8 @@ class Dog(ap.Agent):
         return np.linalg.norm(sheep.pos - self.pos)
 
     def V_front(self, sheep_list, Dcdlr):
+        #if(len(sheep_list))<= 0:
+        #    return None
         H_distances = []
         for sheep in sheep_list:
             H_distances.append(self.H(sheep, Dcdlr))
@@ -114,7 +120,60 @@ class Dog(ap.Agent):
 
         max_index = np.argmax(G_distances)
         return sheep_list[max_index]
+    
+    def V_anchor(self, sheep_list, Dqc):
+        #if(len(sheep_list))<= 0:
+        #    return None
+        F_distances = []
+        for sheep in sheep_list:
+            F_distances.append(self.F(sheep, Dqc))
 
+        min_index = np.argmin(F_distances)
+        return sheep_list[min_index]
+
+    def left_right (self,position, vision):
+        A = unit_vector(position - self.p.destination_center)
+        C = -1 * A
+
+        # Checking the left
+        left_check = True
+        right_check = True
+        
+        for sheep in vision:
+            B = unit_vector(sheep.pos - self.p.destination_center)
+            if np.cross(A, B) >= 0 and np.cross(C, B) < 0:
+                left_check = False
+                sheep.color = 'teal' #right
+            elif np.cross(A, B) < 0 and np.cross(C, B) >= 0:
+                right_check = False
+                sheep.color = 'lavender' #left
+            else:
+                "Oops"
+        return left_check,right_check
+
+    def getLeft_RightSheep(self, vision, A):
+        leftSheepList = []
+        rightSheepList = []
+        
+        
+        #A = unit_vector(position - self.p.destination_center)
+        C = -1 * A
+
+        
+        for sheep in vision:
+            B = unit_vector(sheep.pos - self.p.centroid_sheep)
+            if np.cross(A, B) >= 0 and np.cross(C, B) < 0:
+                
+                rightSheepList.append(sheep)
+            elif np.cross(A, B) < 0 and np.cross(C, B) >= 0:
+                
+                leftSheepList.append(sheep)
+            else:
+                "Oops"
+                
+        if(len(leftSheepList)<= 0 ) or (len(rightSheepList)<=0):
+            print("Error !! no sheep in left or right of sheep centroid!!")
+        return leftSheepList,rightSheepList    
 
     def update_velocity(self):
 
@@ -131,66 +190,60 @@ class Dog(ap.Agent):
         # rho_g: minimal action distance of the attractive effect between two sheep
 
         # Sheepdog vision
-        vision = self.neighbors(self, distance=self.p.rho_vp).to_list()
+        vision = self.neighbors(self, distance=self.p.rho_vq).to_list()
 
         # Calculating centroid of visible sheep (p^c(k))
         centroid_list = []
         for sheep in vision:
             centroid_list.append(sheep.pos)
 
-        centroid = np.mean(centroid_list, axis=0)
-
-        Dcd = unit_vector(np.array(self.p.destination_center) - centroid)
+        self.p.centroid_sheep = np.mean(centroid_list, axis=0)
+        Dcd = unit_vector(np.array(self.p.destination_center) - self.p.centroid_sheep)
         Dcdl = np.matmul(rotation(np.pi / 2), Dcd)
         Dcdr = np.matmul(rotation(-np.pi / 2), Dcd)
-        Dqc = unit_vector(centroid - pos)
+        Dqc = unit_vector(self.p.centroid_sheep - pos)
 
-        A = unit_vector(pos - self.p.destination_center)
-        C = -1 * A
-
-        # Checking the left
-        left_check = True
-        right_check = True
-        for sheep in vision:
-            B = unit_vector(sheep.pos - self.p.destination_center)
-            if np.cross(A, B) >= 0 and np.cross(C, B) < 0:
-                left_check = False
-                sheep.color = 'teal'
-            elif np.cross(A, B) < 0 and np.cross(C, B) >= 0:
-                right_check = False
-                sheep.color = 'lavender'
-            else:
-                "Oops"
-
-
+        left_check,right_check = self.left_right(pos, vision)
         # Calculating critical sheep (V_lf = P_lf under the assumption V_lf is always singleton)
-        V_lf = self.V_front(vision, Dcdl)
-        V_rf = self.V_front(vision, Dcdr)
+        self.V_lf = self.V_front(vision, Dcdl)
+        self.V_rf = self.V_front(vision, Dcdr)
 
-        V_lf.critical[0] = 1
-        V_rf.critical[1] = 1
+        #if self.V_lf != None:
+        self.V_lf.critical[0] = 1
+        self.V_rf.critical[1] = 1
+        
+        #TODO left_rightSheepList
+        leftSheepList, rightSheepList = self.getLeft_RightSheep(vision, Dqc)
+        #TODO anchorSheep 
+        self.V_la = self.V_anchor(leftSheepList, Dqc)
+        self.V_ra = self.V_anchor(rightSheepList, Dqc)
+        
+        self.V_la.critical[2] = 1
+        self.V_ra.critical[3] = 1
         #P_lf = self.P_left_front(vision)
 
-        theta_lt = angle(Dcd, pos - V_lf.pos)
-        theta_rt = angle(Dcd, pos - V_rf.pos)
-
+        theta_lt = angle(Dcd, pos - self.V_lf.pos)
+        theta_rt = angle(Dcd, pos - self.V_rf.pos)
+          
         # Algorithm 1 START
         # u_p: velocity of sheepdog
         u_p = np.zeros(2)
         if left_check and theta_lt < self.p.theta_t:
             # TODO: Anchor rotation right
+            print("Should turn left here - ", self.p.time_step)
             self.state_flag = 1
-            pass
+            u_p = self.p.velocity_coefficient * np.matmul(rotation(- self.p.detouring_theta) , (self.V_ra.pos - pos))
         elif right_check and theta_rt < self.p.theta_t:
             # TODO: Anchor rotation left
+            print("Should turn right here - ", self.p.time_step)
             self.state_flag = -1
-            pass
+            u_p = self.p.velocity_coefficient * np.matmul(rotation(self.p.detouring_theta) , (self.V_la.pos - pos))
         elif self.state_flag == 1:
             # TODO: more
-            pass
+            u_p = self.p.velocity_coefficient * np.matmul(rotation(- self.p.detouring_theta) , (self.V_ra.pos - pos))
         else:
             # TODO: more
-            pass
+            u_p = self.p.velocity_coefficient * np.matmul(rotation(self.p.detouring_theta) , (self.V_la.pos - pos))
 
         # if left_check:
         #     print("All on left, time step: ", self.p.time_step)
@@ -214,7 +267,7 @@ class Dog(ap.Agent):
         # Update velocity using attractive, repulsive, boundary, and noise forces
         # self.velocity += v_pi_attractive + v4 + nv
         # self.velocity += v_pi_repulsive + v4 + nv
-        velocity_vector = border_repulsive
+        velocity_vector = border_repulsive + u_p
         self.velocity += velocity_vector
         self.velocity = saturate(self.velocity, self.p.speed_limit_d)
 
@@ -373,12 +426,12 @@ class SheepModel(ap.Model):
         agent_dog = Dog(self)
         self.agents.insert(1, agent_dog)  # Add dog
 
-        xPos = random.sample(range(0, 100), self.p.population)
-        yPos = random.sample(range(50, 85), self.p.population)
+        xPos = random.sample(range(35, 65), self.p.population)
+        yPos = random.sample(range(55, 85), self.p.population)
         xPos = [float(x) for x in xPos]
         yPos = [float(y) for y in yPos]
         self.positionList = np.column_stack((xPos, yPos))
-        agentListFront = np.append([self.p.destination_center], [(50.0, 50.0)], axis=0)
+        agentListFront = np.append([self.p.destination_center], [(50.0, 35.0)], axis=0)
         self.positionList = np.append(agentListFront, self.positionList, axis=0)
 
         posList = [(x[0], x[1]) for x in self.positionList]
@@ -400,7 +453,9 @@ def animation_plot_single(m, ax):
     pos = m.space.positions.values()
     pos = np.array(list(pos)).T  # Transform
     ax.scatter(*pos, s=1, c='black')
-
+    centroid = plt.Circle((m.p.centroid_sheep[0], m.p.centroid_sheep[1]), radius=5, edgecolor="pink",
+                           facecolor="pink")  # target area
+    ax.add_patch(centroid)
     critical_text = {
         0: "Plf",
         1: "Prf",
@@ -452,18 +507,19 @@ def animation_plot(m, p):
 parameters = {
     'size': 100,
     'seed': 128,
-    'steps': 200,
+    'steps': 1000,
     'population': 24,
     'rho_x': 1.5,
     'rho_s': 1.2,
     'rho_r': 2.2,
     'rho_g': 5.4,
     'rho_vp': 20,
+    'rho_vq': 30,
     'alpha': 0.7,
     'beta': 5,
     'gamma': 0.02,
     'velocity_coefficient': 0.6,
-    'detouring_theta': 0.6,
+    'detouring_theta': ((.6 *360)/np.pi),
     'theta_t': 2.5,
     'theta_b': 0.1,
     'destination_center': (50.0, 87.0),  # p_d
@@ -474,6 +530,7 @@ parameters = {
     'alignment_strength': 0.3,
     'border_strength': 0.5,
     'speed_limit': 0.1,
+    'centroid_sheep': (0.0, 0.0),
     'speed_limit_d': 5.0 / 30.0,
     'time_step': 0
 }
